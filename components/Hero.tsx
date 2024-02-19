7b017b84d38df8d3f64eb175ec37458756b58211
TH-Raju <rajukhan894200@gmail.com>
2024-02-20 01:18:27 +0600

    nav and footer component declare

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import CustomButton from "./CustomButton";
 const Hero = () => {
   const handleScroll = () => {};
@@ -19,6 +20,13 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="photo" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
+        </div>
       </div>
     </div>
   );
