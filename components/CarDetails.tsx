92a561ce51ad7120769dc4bbc10cba34b95c5aa3
TH-Raju <rajukhan894200@gmail.com>
2024-02-20 04:41:40 +0600

    images

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -57,6 +57,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 <div className="flex-1 flex flex-col gap-3">
                   <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                     <Image
+                      //   src={generateCarImageUrl(car, "29")}
                       src="/hero.png"
                       alt="car model"
                       fill
